4ac09952edcd73395e6bb5afcd1110f139dbf33b
khoanna <khoa210105@gmail.com>
2026-07-02 19:58:55 +0700

    feat(be): enable CORS for the web-wallet origin

--- be/src/main.ts
+++ be/src/main.ts
@@ -4,6 +4,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { rawBody: true });
+  app.enableCors({
+    origin: (process.env.WEB_WALLET_ORIGIN ?? 'http://localhost:3001').split(','),
+    credentials: false,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
